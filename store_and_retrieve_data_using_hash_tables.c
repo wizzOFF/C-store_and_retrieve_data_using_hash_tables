8ea9f7383f7f86bfad577105e7084510b2ed5195
WizzOFF <maher_off@proton.me>
2025-03-24 00:22:08 +0100

    store_and_retrieve_data_using_hash_tables.c: write a hash function

--- store_and_retrieve_data_using_hash_tables.c
+++ store_and_retrieve_data_using_hash_tables.c
@@ -5,3 +5,16 @@ Eleve* tableau[100];
 
 sommeLettres % 100
 */
+
+int hachage(char *chaine)
+{
+    int i = 0, nombreHache = 0;
+
+    for (i = 0 ; chaine[i] != '\0' ; i++)
+    {
+        nombreHache += chaine[i];
+    }
+    nombreHache %= 100;
+
+    return nombreHache;
+}
